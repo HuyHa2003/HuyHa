01a5d044d812c7f4035331ba7eedfb2c37631171
HuyHa2003 <125466463+HuyHa2003@users.noreply.github.com>
2023-03-05 16:15:53 +0700

    Add files via upload

---
+++ VuHaHuy_Project/VuHaHuy_App.java
@@ -0,0 +1,9 @@
+package hellojava;
+public class VuHaHuy_App {
+
+	public static void main(String[] args) {
+		System.out.println("Vu Ha Huy hoc Java");
+
+	}
+
+}
---
+++ VuHaHuy_Project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Huy
+ *
+ */
+module VuHaHuy_Project {
+}
