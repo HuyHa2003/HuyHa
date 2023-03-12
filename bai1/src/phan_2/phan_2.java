bcb52dc33c5043da324b26924cd186ed9349aa80
HuyHa2003 <125466463+HuyHa2003@users.noreply.github.com>
2023-03-12 23:35:34 +0700

    Add files via upload

---
+++ bai1/src/phan_2/phan_2.java
@@ -0,0 +1,22 @@
+package phan_2;
+
+import java.util.Scanner;
+
+public class phan_2 {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số thứ nhất: ");
+        int a = scanner.nextInt();
+        System.out.print("Nhập số thứ hai: ");
+        int b = scanner.nextInt();
+
+        // So sánh hai số với nhau và in kết quả ra màn hình
+        System.out.println(a + " == " + b + " là " + (a == b));
+        System.out.println(a + " != " + b + " là " + (a != b));
+        System.out.println(a + " > " + b + " là " + (a > b));
+        System.out.println(a + " < " + b + " là " + (a < b));
+        System.out.println(a + " >= " + b + " là " + (a >= b));
+        System.out.println(a + " <= " + b + " là " + (a <= b));
+    }
+
+}
